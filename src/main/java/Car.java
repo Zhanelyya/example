95d0f9aa7ed91dae50711897a0807b338ac8f1dc
Zhanelyya <bayganina_b@mail.ru>
2024-02-11 02:51:25 +0500

    first commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Car: " + brand + " " + model + ", Year: " + year);
+    }
+}
