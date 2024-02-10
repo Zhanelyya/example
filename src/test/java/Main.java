bbd22fb80f9d7ce382125ee4190bc12457a1c95c
Zhanelyya <bayganina_b@mail.ru>
2024-02-11 03:31:40 +0500

    add a test version of class

---
+++ src/test/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Создание объекта класса Car
+        Car myCar = new Car("Toyota", "Corolla", 2020);
+
+        // Вызов метода displayInfo() для вывода информации о машине
+        myCar.displayInfo();
+    }
+}
