5deeea64e9ff00faea6727c015ff09a8290f2bcd
Zhanelyya <bayganina_b@mail.ru>
2024-02-11 03:47:05 +0500

    add a new elements for two class

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -2,13 +2,15 @@ public class Car {
     String brand;
     String model;
     int year;
+    int number ;
 
-    public Car(String brand, String model, int year) {
+    public Car(String brand, String model, int year, int number) {
         this.brand = brand;
         this.model = model;
         this.year = year;
+        this.number = number;
     }
     public void displayInfo() {
-        System.out.println("Car: " + brand + " " + model + ", Year: " + year);
+        System.out.println("Car: " + brand + " " + model + ", Year: " + year + ", Number: " + number);
     }
 }
